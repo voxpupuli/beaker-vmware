24a4ea12b458f5ba50592b97e51e1338969e8bce
Tim Meusel <tim@bastelfreak.de>
2023-03-24 16:26:08 +0100

    spec_helper.rb: Apply patches from beaker-docker

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,21 @@
+require 'beaker'
+
 begin
   require 'simplecov'
   require 'simplecov-console'
   require 'codecov'
 rescue LoadError
+  # Do nothing if no required gem installed
 else
   SimpleCov.start do
     track_files 'lib/**/*.rb'
 
     add_filter '/spec'
-
-    enable_coverage :branch
-
     # do not track vendored files
     add_filter '/vendor'
     add_filter '/.vendor'
+
+    enable_coverage :branch
   end
 
   SimpleCov.formatters = [
@@ -22,10 +24,7 @@ else
   ]
 end
 
-require 'rspec/its'
-require 'beaker'
-
-Dir.glob(Dir.pwd + '/lib/beaker/hypervisor/*.rb') {|file| require file}
+Dir['./lib/beaker/hypervisor/*.rb'].sort.each { |file| require file }
 
 # setup & require beaker's spec_helper.rb
 beaker_gem_spec = Gem::Specification.find_by_name('beaker')
